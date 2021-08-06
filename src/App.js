08efd5c5c081e329f7fe4c174be92c1a1cf4d1d0
blvckcenturion <mr.plus.co@gmail.com>
2021-08-05 20:11:11 -0400

    iAdded home page animations & basic implementation of navigation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Header from "./components/header";
-import Banner from './components/banner';
-import Cases from './components/cases';
 import "./styles/App.scss";
-import gsap from 'gsap';
-import IntroOverlay from "./components/introOverlay";
+import gsap from "gsap";
+import {Route} from 'react-router-dom';
+import Navigation from "./components/navigation";
+//pages 
+import Home from "./pages/home";
+import CaseStudies from "./pages/caseStudies";
+import Approach from "./pages/approach";
+import Services from "./pages/services";
+import About from "./pages/about";
+
+
+//routes
+
+const routes = [
+  {path: '/', name: 'home', component: Home},
+  {path: '/case-studies', name: 'Case Studies', component: CaseStudies},
+  {path: '/approach', name: 'Approach', component: Approach},
+  {path: '/services', name: 'Services', component: Services},
+  {path: '/about', name: 'About Us', component: About},
+]
 
 function App() {
 
   useEffect(() => {
+    const tl = gsap.timeline();
+    //making the body visible
+
     let vh = window.innerHeight * .01;
     document.documentElement.style.setProperty('--vh', `${vh}px`)
 
-    const tl = gsap.timeline();
-
-    tl.from(".line span", 1, {
-      y: 100,
-      ease: "power4.out",
-      delay:1,
-      skewY: 10,
-      stagger: {
-        amount: .3
-      }
-    }) 
+    tl.to("body", 0, {css: {visibility: "visible"}})
   })
+
+
   
   return (
-    <div className='App'>
-      <IntroOverlay/>
+    <>
       <Header/>
-      <Banner/>
-      <Cases/>
-    </div>
+      <div className='App'>
+        {routes.map(({path, component}) => (<Route key={path} exact path={path} component={component}/>))}
+      </div>
+      <Navigation/>
+    </>
   );
 }
 
